3ab7bb9c870490161ed5407025a386f0085ea60a
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 17:47:19 -0500

    ADD: Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <Navbar />
         <h1>Hello</h1>
       </div>
     );
