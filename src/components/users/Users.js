a834991661ad6e98632b444138503bf0892f06f7
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 20:17:37 -0500

    ADD: Spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  render() {
-    const { users } = this.props;
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
         {users.map((user) => (
@@ -12,12 +15,17 @@ class Users extends Component {
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem',
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default Users;
