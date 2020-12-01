019d02774a1bd3d0787021147870d4f008bbdb50
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 19:24:03 -0500

    CHNGE: Class to Functional

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-  state = {
-    id: '1',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
+const UserItem = ({ user }) => {
+  const { login, avatar_url, html_url } = user;
 
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm">
-            Open In Github
-          </a>
-        </div>
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm">
+          Open In Github
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
