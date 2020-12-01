da8397c43901e0295af5b367a04fa8c4e4471ea1
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-12-01 18:33:12 -0500

    ADD: HOOKS

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   searchUsers = async (text) => {
@@ -36,6 +37,16 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_GITHUB_CLIENT}&client_secret=${process.env.REACT_GITHUB_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
@@ -47,7 +58,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -76,7 +87,14 @@ class App extends Component {
                 exact
                 path="/user/:login"
                 render={(props) => (
-                  <User {...props} getUser={this.getUser} user={user} loading />
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
                 )}
               />
             </Switch>
