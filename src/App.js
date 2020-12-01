a3a53b3826456796c09c80d2236ffc6c4f7c8b0e
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-12-01 13:02:42 -0500

    ADD: Clear Button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -11,17 +12,13 @@ class App extends Component {
     loading: false,
   };
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_GITHUB_CLIENT}$client_secret=${process.env.REACT_GITHUB_SECRET}`
-  //   );
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
 
   searchUsers = async (text) => {
+    this.setState({ loading: true });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_GITHUB_CLIENT}$client_secret=${process.env.REACT_GITHUB_SECRET}`
     );
