a9cb3c5938e79795cc59c023af7c9749f9bf2d5a
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-12-02 12:30:09 -0500

    DONE: APP

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,17 @@ import {
   GET_REPOS,
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_GITHUB_CLIENT;
+  githubClientSecret = process.env.REACT_GITHUB_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT;
+  githubClientSecret = process.env.GITHUB_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -26,7 +37,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_GITHUB_CLIENT}&client_secret=${process.env.REACT_GITHUB_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -41,7 +52,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_GITHUB_CLIENT}&client_secret=${process.env.REACT_GITHUB_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -56,7 +67,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_GITHUB_CLIENT}&client_secret=${process.env.REACT_GITHUB_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
